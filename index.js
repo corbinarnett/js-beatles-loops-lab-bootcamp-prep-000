5744d7a79982f677e0d96dab60d0b39fa8d892c5
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-04 20:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < musicians.length; i++) {
-        let currentMusician = 
+        let currentMusician = musicians[i];
+        let currentInstrument = instruments[i];
 }
