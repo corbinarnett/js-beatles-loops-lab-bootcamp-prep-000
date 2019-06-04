00efd44edec27ac4e90dd232392ae3ac17bd6f41
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-04 21:11:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function johnLennonFacts(factsArray) {
 function iLoveTheBeatles(num) {
   var array = [];
     do {
-    array.push("I love the Beatles!")
-    num++
+    array.push("I love the Beatles!");
+    num++;
   } while (num < 15);
-  return array
+  return array;
   }
 }
